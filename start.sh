058da203e326343491808b3b903f5d582dc421b2
julialejah <50000959+julialejah@users.noreply.github.com>
2022-03-23 14:27:17 -0500

    Update and rename buildcontainerimages.sh to start.sh

--- start.sh
+++ start.sh
@@ -2,3 +2,6 @@
 #Build images from dockerfile
 sudo docker build --tag=scapy -f ../example-containers/scapy/Dockerfile  ../example-containers/scapy
 sudo docker build --tag=odl -f ../example-containers/odl/Dockerfile  ../example-containers/odl
+
+#Create directory for the dockers in file "red2.py"
+sudo mkdir /home/juli/shvol
