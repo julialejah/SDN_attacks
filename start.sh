21089da280ff5315dc65e74b468dbb00decf9fb9
julialejah <50000959+julialejah@users.noreply.github.com>
2022-05-02 18:16:49 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,10 @@
 
 #Build images from dockerfile
-sudo docker build --tag=scapy -f ../example-containers/scapy/Dockerfile  ../example-containers/scapy
-sudo docker build --tag=odl -f ../example-containers/odl/Dockerfile  ../example-containers/odl
+sudo docker build --tag=scapy -f containernet/Dockerfile/scapy/Dockerfile  containernet/Dockerfile/scapy
+sudo docker build --tag=odl_f -f containernet/Dockerfile/odl/Dockerfile  containernet/Dockerfile/odl
 
 #Create directory for the dockers in file "red2.py"
 sudo mkdir /home/juli/shvol
+
+#run the odl docker
+sudo docker run -d -p 6633:6633 -p 8101:8101 -p 8181:8181 --name=odl odl_f
