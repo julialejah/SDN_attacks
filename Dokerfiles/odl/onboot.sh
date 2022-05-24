af782fda12f3b53d95d655f669c36d0123cb479d
julialejah <50000959+julialejah@users.noreply.github.com>
2022-05-24 13:15:34 -0500

    Create onboot.sh

---
+++ Dokerfiles/odl/onboot.sh
@@ -0,0 +1,3 @@
+/etc/init.d/ssh restart
+karaf-0.8.4/bin/start
+karaf-0.8.4/bin/client feature:install odl-dlux-core odl-openflowplugin-libraries odl-mdsal-all odl-l2switch-all odl-restconf-all odl-yangtools-common
