f9e3363a21f0bf3c3595fab1afb993a9db25c1c0
julialejah <50000959+julialejah@users.noreply.github.com>
2022-08-23 14:42:19 -0500

    Update run_odl.sh

--- run_odl.sh
+++ run_odl.sh
@@ -1,1 +1,2 @@
+sudo docker build -t odl -f Dockerfiles/odl/Dockerfile Dockerfiles/odl/
 sudo docker run -t -d -p 6633:6633 -p 6653:6653 -p 8181:8181 -p 8101:8101 -p 2222:22 --name odl --privileged odl 
