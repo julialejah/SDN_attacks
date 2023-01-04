4281a7d7b96c7faa0e743c77fd1d7c7e606ca327
julialejah <julianalejandra@gmail.com>
2023-01-04 12:27:57 -0500

    start.sh con odl run ok

--- start.sh
+++ start.sh
@@ -7,4 +7,4 @@ sudo docker build --tag=odl_f -f Dockerfiles/odl/Dockerfile  Dockerfiles/odl
 sudo mkdir /home/juli/shvol
 
 #run the odl docker
-sudo docker run -d -p 6633:6633 -p 8101:8101 -p 8181:8181 --name=odl odl_f
+sudo docker run -t -d -p 6633:6633 -p 6653:6653 -p 8181:8181 -p 8101:8101 -p 2222:22 --name odl --privileged odl 
